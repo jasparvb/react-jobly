512fa8e03a75a45a02527792355c5c130c244566
von Buelow <jaspar@UMICH.EDU>
2020-07-25 16:24:29 -0400

    Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Routes from './Routes'
 
 function App() {
   return (
     <div className="App">
+      <Routes/>
     </div>
   );
 }
---
+++ src/Routes.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from './Home';
+import Companies from './Companies';
+import Company from './Company';
+import Jobs from './Jobs';
+import Login from './Login';
+import Profile from './Profile';
+
+function Routes({companies, jobs}) {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/companies">
+        <Companies companies={companies} />
+      </Route>
+      <Route path="/companies/:company">
+        <Company company={companies} />
+      </Route>
+      <Route exact path="/jobs">
+        <Jobs companies={jobs} />
+      </Route>
+      <Route exact path="/login">
+        <Login companies={jobs} />
+      </Route>
+      <Route exact path="/profile">
+        <Profile companies={jobs} />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  );
+}
+
+export default Routes;
