2ac0f5e941e302f194e98f812cbd99332ed6195a
von Buelow <jaspar@UMICH.EDU>
2020-07-28 22:26:02 -0400

    Toggle login and sign up

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from "react";
 
-
 function Login() {
+    const [activeTab, setActiveTab] = useState('login');
+
+    function setLogin() {
+        setActiveTab('login');
+    }
+    function setSignup() {
+        setActiveTab('signup');
+    }
+    const signupFields = (
+        <div>
+            <div className="form-group">
+                <label>First name</label>
+                <input name="first_name" className="form-control" value="" />
+            </div>
+            <div className="form-group">
+                <label>Last name</label>
+                <input name="last_name" className="form-control" value="" />
+            </div>
+            <div className="form-group">
+                <label>Email</label>
+                <input type="email" name="email" className="form-control" value="" />
+            </div>
+        </div>
+    );
+
     return (
         <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
             <div className="d-flex justify-content-end">
                 <div className="btn-group">
-                    <button className="btn btn-primary active">Login</button>
-                    <button className="btn btn-primary">Sign up</button>
+                    <button className={`btn btn-primary ${activeTab === 'login' ? "active" : ""}`} onClick={setLogin}>Login</button>
+                    <button className={`btn btn-primary ${activeTab === 'signup' ? "active" : ""}`} onClick={setSignup}>Sign up</button>
                 </div>
             </div>
             <div className="card">
@@ -21,20 +45,7 @@ function Login() {
                             <label>Password</label>
                             <input type="password" name="password" className="form-control" value="" />
                         </div>
-                        <div>
-                            <div class="form-group">
-                                <label>First name</label>
-                                <input name="first_name" class="form-control" value="" />
-                            </div>
-                            <div class="form-group">
-                                <label>Last name</label>
-                                <input name="last_name" class="form-control" value="" />
-                            </div>
-                            <div class="form-group">
-                                <label>Email</label>
-                                <input type="email" name="email" class="form-control" value="" />
-                            </div>
-                        </div>
+                        {activeTab === 'signup' ? signupFields : ''}
                         <button type="submit" className="btn btn-primary float-right">Submit</button>
                     </form>
                 </div>
