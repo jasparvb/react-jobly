b956e22c34a04c5f64f064bb2805092463006ecd
von Buelow <jaspar@UMICH.EDU>
2020-07-30 08:29:02 -0400

    Profile form fields

--- src/Profile.js
+++ src/Profile.js
@@ -50,7 +50,72 @@ function Profile() {
       }));
     };
   
-
+    return (
+        <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+            <div className="card">
+                <div className="card-body">
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label>Username</label>
+                            <p className="form-control-plaintext">{userData.username}</p>
+                        </div>
+                        <div className="form-group">
+                            <label>First name</label>
+                            <input 
+                                name="first_name" 
+                                className="form-control" 
+                                value={userData.first_name} 
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Last name</label>
+                            <input 
+                                name="last_name" 
+                                className="form-control" 
+                                value={userData.last_name} 
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input 
+                                type="email" 
+                                name="email" 
+                                className="form-control" 
+                                value={userData.email} 
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Photo URL</label>
+                            <input 
+                                type="photo_url" 
+                                name="photo_url" 
+                                className="form-control" 
+                                value={userData.photo_url} 
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Re-enter Password</label>
+                            <input 
+                                type="password" 
+                                name="password" 
+                                className="form-control" 
+                                value={userData.password} 
+                                onChange={handleChange}
+                            />
+                        </div>
+                        {userData.errors.length ? (
+                            <Alert type="danger" messages={userData.errors} />
+                        ) : null}
+                        <button type="submit" className="btn btn-primary btn-block mt-4">Save Changes</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Profile;
