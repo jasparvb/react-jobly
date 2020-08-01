63b20800f7dcac9dc787698ea6a9d3a5d0865111
von Buelow <jaspar@UMICH.EDU>
2020-08-01 16:17:10 -0400

    Fix login link

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import UserContext from "./UserContext";
+import { Link } from "react-router-dom";
 
 function Home() {
   const { user } = useContext(UserContext);
@@ -8,7 +9,7 @@ function Home() {
       <div className="container text-center">
         <h1 className="mb-4 font-weight-bold">Jobly</h1>
         <p className="lead">All the jobs in one, convenient place.</p>
-        {user ? <h2>Welcome Back!</h2> : <a className="btn btn-primary font-weight-bold" href="/login">Log in</a>}
+        {user ? <h2>Welcome Back!</h2> : <Link className="btn btn-primary font-weight-bold" to="/login">Log in</Link>}
       </div>
     </section>
   );
